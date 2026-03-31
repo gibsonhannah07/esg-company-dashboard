d583d8c40a9ae332f01a838d5c8707498b3e664b
gibsonhannah07 <gibsonhannah07@gmail.com>
2026-03-31 15:13:06 -0500

    created filter bar feature that allows users to view companies by industry, just displays options received as props

--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 
-export default function () {
+export default function ({ industries, selected, onFilterChange }) {
   return (
-    <div className="filter-bar">
-      <p>[filter bar will go here]</p>
+    <div>
+      <label htmlFor="industry-filter">Filter by Industry:</label>
+      <select
+        id="industry-filter"
+        value={selected}
+        onChange={(e) => onFilterChange(e.target.value)}
+      >
+        {industries.map((industry) => (
+          <option key={industry} value={industry}>
+            {industry}
+          </option>
+        ))}
+      </select>
     </div>
-  )
+  );
 }
